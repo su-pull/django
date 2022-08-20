614ae6085dfd7f2aad4bcd338e48ffd95c7a30ea
sou <ocelot2828@gmail.com>
2022-08-20 17:21:01 +0900

    second commit

--- api/models.py
+++ api/models.py
@@ -6,11 +6,11 @@ class Docs(models.Model):
 
     title_ja = models.CharField(max_length=100)
     content_ja = models.TextField()
-    description_ja = models.TextField()
+    description_ja = models.TextField(default="")
     title_en = models.CharField(max_length=100)
-    content_en = models.TextField()
+    content_en = models.TextField(default="")
     description_en = models.TextField()
-    slug = models.SlugField()
+    slug = models.SlugField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
