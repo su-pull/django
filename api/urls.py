4a2e485c2040f9ad0c96dd2d54a44399d9b22d07
sou <ocelot2828@gmail.com>
2022-08-20 19:41:43 +0900

    second commit

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('list-docs', DocsListView.as_view(), name='list-docs'),
     path('list-docs/<slug:slug>/', DocsRetrieveView.as_view(), name='docs_detail'),
-    path('list-talk', TalkListView.as_view(), name='list-post'),
+    path('list-talk', TalkListView.as_view(), name='list-talk'),
     path('list-talk/<slug:slug>/', TalkRetrieveView.as_view(), name='talk_detail'),
     path('register/', CreateUserView.as_view(), name='register'),
     path('auth/', include('djoser.urls.jwt')),
