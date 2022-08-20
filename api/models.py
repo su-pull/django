22f1ded31fac28af9d9bda6a717044c1ee263d2e
sou <ocelot2828@gmail.com>
2022-08-20 19:57:40 +0900

    second commit

--- api/models.py
+++ api/models.py
@@ -24,10 +24,10 @@ class Talk(models.Model):
 
     title_ja = models.CharField(max_length=100)
     content_ja = models.TextField()
-    description_ja = models.TextField(default="")
+    description_ja = models.TextField(null=True, blank=True, default="")
     title_en = models.CharField(max_length=100)
     content_en = models.TextField()
-    description_en = models.TextField(default="")
+    description_en = models.TextField(null=True, blank=True, default="")
     slug = models.SlugField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
