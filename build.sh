58f77b3bdea3e868bfcaa1768f7a0c9320ac5e30
sou <ocelot2828@gmail.com>
2022-10-24 14:58:22 +0900

    push

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --email admin@test.com --noinput
+python manage.py createsuperuser --username sou --email ocelot2828@gmail.com --noinput
